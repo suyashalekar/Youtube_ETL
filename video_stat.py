94a45945dc9044f0b18573acb5e63afa3e8297c7
suyashalekar <suyashalekar519@gmail.com>
2025-11-27 14:27:45 +0530

    added video id

--- video_stat.py
+++ video_stat.py
@@ -7,6 +7,7 @@ load_dotenv(dotenv_path="./.env")  # loads the .env file automatically
 
 api_key = os.getenv("API_KEY")
 CHANNEL_HANDLE = "MrBeast"
+max_result = 50
 
 def get_playlist_id():
 
@@ -36,7 +37,45 @@ def get_playlist_id():
     except requests.exceptions.RequestException as e:
         raise e
     
+def get_video_ids(playlist_id):
 
+    base_url = f"https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults={max_result}&playlistId={playlist_id}&key={api_key}"
+
+
+    video_id_list = []
+    page_token = None 
+
+    try:
+        while True: 
+             
+            url = base_url
+
+            if page_token:
+                url += f"&pageToken={page_token}"
+
+            response = requests.get(url)
+
+            response.raise_for_status()
+
+            data = response.json()
+
+            for item in data.get('items',[]):
+                video_id = item['contentDetails']['videoId']
+                video_id_list.append(video_id)
+            
+
+            page_token = data.get('nextPageToken')
+
+            if not page_token:
+                break
+
+        return video_id_list
+    
+    except requests.exceptions.RequestException as e:
+        raise e
+
+    
 
 if __name__ == "__main__":
-    get_playlist_id()
+    playlist_id = get_playlist_id()
+    get_video_ids(playlist_id)
